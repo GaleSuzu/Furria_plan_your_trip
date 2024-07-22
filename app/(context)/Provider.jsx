e3630e538555922fb8944cadc6721bd1f9fcdb75
Ssuanna Palmeri <susanna.palmeri87@gmail.com>
2024-07-22 20:33:51 +0200

    first commit

---
+++ app/(context)/Provider.jsx
@@ -0,0 +1,15 @@
+"use client";
+
+import { createContext, useState } from "react";
+export const globalContext = createContext();
+
+const Context = ({ children }) => {
+  const [city, setCity] = useState([]); //qui mettiamo tutti gli useState che ci serve portare in giro
+
+  const value = { city, setCity };
+  return (
+    <globalContext.Provider value={value}>{children}</globalContext.Provider>
+  );
+};
+
+export default Context;

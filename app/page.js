cee272e6ecf22b5d78ccaee73c251f1db6b07bf1
robertafavuzza <roberta.favuzza98@gmail.com>
2024-07-23 18:38:03 +0200

    Implementazione della schermata di caricamento con logo e barra di avanzamento

--- app/page.js
+++ app/page.js
@@ -1,12 +1,23 @@
-import styles from "./page.module.css";
-import About from "./about/page";
+'use client'; 
+
+import { useState } from 'react';
+import LoadingStartApp from './components/loading-startapp/LoadingStartApp';
+import styles from './page.module.css';
 
 export default function Home() {
+  const [loading, setLoading] = useState(true);
+
+  const handleLoadingComplete = () => {
+    setLoading(false);
+  };
+
+  if (loading) {
+    return <LoadingStartApp onLoadingComplete={handleLoadingComplete} />;
+  }
+
   return (
     <main className={styles.main}>
       <p>Ciao sono main</p>
     </main>
   );
 }
-
-//qui mettiamo tutti i componenti che ci serve montare

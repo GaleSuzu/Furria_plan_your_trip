ffce14274701697448e557be4a2694ae1bcbc9c2
robertafavuzza <roberta.favuzza98@gmail.com>
2024-07-23 21:21:17 +0200

    Verifica e correzione del caricamento della pagina

--- app/page.js
+++ app/page.js
@@ -1,7 +1,24 @@
 'use client';
 
-import MainPage from './components/home/Home';
+import { useState, useEffect } from 'react';
+import LoadingStartApp from './components/loading-startapp/LoadingStartApp';
+import Home from './components/home/Home';
+import './globals.css'; 
 
-export default function Home() {
-  return <MainPage />;
+export default function MainPage() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 3000); 
+
+    return () => clearTimeout(timer); 
+  }, []);
+
+  if (loading) {
+    return <LoadingStartApp />;
+  }
+
+  return <Home />;
 }

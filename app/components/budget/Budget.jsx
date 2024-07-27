5e34390489067a590f624b775e469cae4721e856
GiuseppeSonny <umanella23@gmail.com>
2024-07-27 18:05:00 +0200

    add notes component

--- app/components/budget/Budget.jsx
+++ app/components/budget/Budget.jsx
@@ -1,10 +1,29 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./budget.module.scss";
 import WalletModal from "../walletModal/modal";
 import FakeWalletCard from "../fakeWalletCard/FakeWalletCard";
+import WalletCard from "../walletCard/walletCard";
 
 const Budget = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [budget, setBudget] = useState({
+    initial: 2000,
+    spent: 0,
+  });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("/api/cost");
+        const data = await response.json();
+        setBudget(data);
+      } catch (error) {
+        console.error("Error fetching budget data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
 
   const openModal = () => {
     setIsModalOpen(true);
@@ -22,6 +41,7 @@ const Budget = () => {
           <input
             type="number"
             className={styles.budgetInput}
+            value={budget.initial}
           />
         </div>
         <div className={styles.budgetSpent}>
@@ -30,6 +50,7 @@ const Budget = () => {
       </div>
       <div className={styles.budgetContent}>
         <FakeWalletCard />
+        <WalletCard />
       </div>
       {isModalOpen && <WalletModal onClose={closeModal} />}
       <button className={styles.addButton} onClick={openModal}>
---
+++ app/components/noteCard/noteCard.js
@@ -0,0 +1,10 @@
+const NoteCard = () => {
+  return (
+    <div>
+      <h2>Titolo della nota</h2>
+      <p>Contenuto della nota</p>
+    </div>
+  );
+};
+
+export default NoteCard;

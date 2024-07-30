7c18e81d31ded86d0bc2f9ae17a23e83b5fd6bee
robertafavuzza <roberta.favuzza98@gmail.com>
2024-07-30 17:23:54 +0200

    Fix modale css

--- app/components/walletModal/WalletModal.js
+++ app/components/walletModal/WalletModal.js
@@ -56,10 +56,14 @@ const WalletModal = ({ cityId, onClose }) => {
             <label>Costo:</label>
             <input type="number" onChange={(e) => setCost(e.target.value)} />
           </div>
-          <label>testo:</label>
-          <input type="text" onChange={(e) => setText(e.target.value)} />
-          <label>category:</label>
-          <input type="text" onChange={(e) => setCategory(e.target.value)} />
+          <div className={styles.formGroup}>
+            <label>Testo:</label>
+            <input type="text" onChange={(e) => setText(e.target.value)} />
+          </div>
+          <div className={styles.formGroup}>
+            <label>Categoria:</label>
+            <input type="text" onChange={(e) => setCategory(e.target.value)} />
+          </div>
           <div className={styles.buttonGroup}>
             <button className={styles.cancelButton} onClick={onClose}>
               Annulla
